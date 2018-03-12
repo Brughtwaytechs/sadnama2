49581de98090f2d15d4526d2fe9caf015d060115
brightwaytechs <brightwaytechs@gmail.com>
2018-03-12 04:50:49 -0700

    ایجاد ظاهر اولیه

--- sadnama2/Form1.Designer.cs
+++ sadnama2/Form1.Designer.cs
@@ -29,11 +29,246 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Node4");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Node5");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Node9");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Node10");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Node11");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Node12");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Node6", new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode4,
+            treeNode5,
+            treeNode6});
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Node7");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Node8");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("فیلم", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode7,
+            treeNode8,
+            treeNode9});
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Node25");
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Node26");
+            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Node27");
+            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("موسیقی", new System.Windows.Forms.TreeNode[] {
+            treeNode11,
+            treeNode12,
+            treeNode13});
+            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("Node22");
+            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("Node23");
+            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("Node24");
+            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("Node13");
+            System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("Node17");
+            System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("Node20");
+            System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("Node21");
+            System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("Node18", new System.Windows.Forms.TreeNode[] {
+            treeNode20,
+            treeNode21});
+            System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("Node19");
+            System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("Node14", new System.Windows.Forms.TreeNode[] {
+            treeNode19,
+            treeNode22,
+            treeNode23});
+            System.Windows.Forms.TreeNode treeNode25 = new System.Windows.Forms.TreeNode("Node15");
+            System.Windows.Forms.TreeNode treeNode26 = new System.Windows.Forms.TreeNode("Node16");
+            System.Windows.Forms.TreeNode treeNode27 = new System.Windows.Forms.TreeNode("Node3", new System.Windows.Forms.TreeNode[] {
+            treeNode18,
+            treeNode24,
+            treeNode25,
+            treeNode26});
+            System.Windows.Forms.TreeNode treeNode28 = new System.Windows.Forms.TreeNode("کلیپ", new System.Windows.Forms.TreeNode[] {
+            treeNode15,
+            treeNode16,
+            treeNode17,
+            treeNode27});
+            this.treeView2 = new System.Windows.Forms.TreeView();
+            this.tabPage1 = new System.Windows.Forms.TabControl();
+            this.tabMain = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.stripMenuItemAdd = new System.Windows.Forms.ToolStripMenuItem();
+            this.stripMenuItemRename = new System.Windows.Forms.ToolStripMenuItem();
+            this.stripMenuItemRemove = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabPage1.SuspendLayout();
+            this.tabMain.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView2
+            // 
+            this.treeView2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.treeView2.Location = new System.Drawing.Point(611, 3);
+            this.treeView2.Name = "treeView2";
+            treeNode1.Name = "Node4";
+            treeNode1.Text = "Node4";
+            treeNode2.Name = "Node5";
+            treeNode2.Text = "Node5";
+            treeNode3.Name = "Node9";
+            treeNode3.Text = "Node9";
+            treeNode4.Name = "Node10";
+            treeNode4.Text = "Node10";
+            treeNode5.Name = "Node11";
+            treeNode5.Text = "Node11";
+            treeNode6.Name = "Node12";
+            treeNode6.Text = "Node12";
+            treeNode7.Name = "Node6";
+            treeNode7.Text = "Node6";
+            treeNode8.Name = "Node7";
+            treeNode8.Text = "Node7";
+            treeNode9.Name = "Node8";
+            treeNode9.Text = "Node8";
+            treeNode10.Name = "Node1";
+            treeNode10.Text = "فیلم";
+            treeNode11.Name = "Node25";
+            treeNode11.Text = "Node25";
+            treeNode12.Name = "Node26";
+            treeNode12.Text = "Node26";
+            treeNode13.Name = "Node27";
+            treeNode13.Text = "Node27";
+            treeNode14.Name = "Node1";
+            treeNode14.Text = "موسیقی";
+            treeNode15.Name = "Node22";
+            treeNode15.Text = "Node22";
+            treeNode16.Name = "Node23";
+            treeNode16.Text = "Node23";
+            treeNode17.Name = "Node24";
+            treeNode17.Text = "Node24";
+            treeNode18.Name = "Node13";
+            treeNode18.Text = "Node13";
+            treeNode19.Name = "Node17";
+            treeNode19.Text = "Node17";
+            treeNode20.Name = "Node20";
+            treeNode20.Text = "Node20";
+            treeNode21.Name = "Node21";
+            treeNode21.Text = "Node21";
+            treeNode22.Name = "Node18";
+            treeNode22.Text = "Node18";
+            treeNode23.Name = "Node19";
+            treeNode23.Text = "Node19";
+            treeNode24.Name = "Node14";
+            treeNode24.Text = "Node14";
+            treeNode25.Name = "Node15";
+            treeNode25.Text = "Node15";
+            treeNode26.Name = "Node16";
+            treeNode26.Text = "Node16";
+            treeNode27.Name = "Node3";
+            treeNode27.Text = "Node3";
+            treeNode28.Name = "Node2";
+            treeNode28.Text = "کلیپ";
+            this.treeView2.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode10,
+            treeNode14,
+            treeNode28});
+            this.treeView2.RightToLeftLayout = true;
+            this.treeView2.Size = new System.Drawing.Size(185, 405);
+            this.treeView2.TabIndex = 1;
+            this.treeView2.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView2_NodeMouseClick);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tabMain);
+            this.tabPage1.Controls.Add(this.tabPage2);
+            this.tabPage1.Controls.Add(this.tabPage3);
+            this.tabPage1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabPage1.Location = new System.Drawing.Point(0, 0);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.tabPage1.RightToLeftLayout = true;
+            this.tabPage1.SelectedIndex = 0;
+            this.tabPage1.Size = new System.Drawing.Size(807, 437);
+            this.tabPage1.TabIndex = 2;
+            // 
+            // tabMain
+            // 
+            this.tabMain.Controls.Add(this.treeView2);
+            this.tabMain.Location = new System.Drawing.Point(4, 22);
+            this.tabMain.Name = "tabMain";
+            this.tabMain.Padding = new System.Windows.Forms.Padding(3);
+            this.tabMain.Size = new System.Drawing.Size(799, 411);
+            this.tabMain.TabIndex = 0;
+            this.tabMain.Text = "صفحه اصلی";
+            this.tabMain.UseVisualStyleBackColor = true;
+            this.tabMain.Click += new System.EventHandler(this.tabMain_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(799, 411);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "مدیریت";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(799, 411);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "آمار";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stripMenuItemAdd,
+            this.stripMenuItemRename,
+            this.stripMenuItemRemove});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(116, 70);
+            // 
+            // stripMenuItemAdd
+            // 
+            this.stripMenuItemAdd.Name = "stripMenuItemAdd";
+            this.stripMenuItemAdd.Size = new System.Drawing.Size(115, 22);
+            this.stripMenuItemAdd.Text = "افزودن";
+            this.stripMenuItemAdd.Click += new System.EventHandler(this.stripMenuItemAdd_Click);
+            // 
+            // stripMenuItemRename
+            // 
+            this.stripMenuItemRename.Name = "stripMenuItemRename";
+            this.stripMenuItemRename.Size = new System.Drawing.Size(115, 22);
+            this.stripMenuItemRename.Text = "تغییر نام";
+            this.stripMenuItemRename.Click += new System.EventHandler(this.stripMenuItemRename_Click);
+            // 
+            // stripMenuItemRemove
+            // 
+            this.stripMenuItemRemove.Name = "stripMenuItemRemove";
+            this.stripMenuItemRemove.Size = new System.Drawing.Size(115, 22);
+            this.stripMenuItemRemove.Text = "حذف";
+            this.stripMenuItemRemove.Click += new System.EventHandler(this.stripMenuItemRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 437);
+            this.Controls.Add(this.tabPage1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabPage1.ResumeLayout(false);
+            this.tabMain.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeView2;
+        private System.Windows.Forms.TabControl tabPage1;
+        private System.Windows.Forms.TabPage tabMain;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem stripMenuItemAdd;
+        private System.Windows.Forms.ToolStripMenuItem stripMenuItemRename;
+        private System.Windows.Forms.ToolStripMenuItem stripMenuItemRemove;
     }
 }
 
--- sadnama2/Form1.cs
+++ sadnama2/Form1.cs
@@ -12,9 +12,77 @@ namespace sadnama2
 {
     public partial class Form1 : Form
     {
-        public Form1()
+      string clickedNode;
+    MenuItem myMenuItem = new MenuItem("Show Me");
+    ContextMenu mnu = new ContextMenu();
+    public Form1()
+    {
+        InitializeComponent();
+        mnu.MenuItems.Add(myMenuItem);
+        myMenuItem.Click += new EventHandler(myMenuItem_Click);
+    }
+
+    void myMenuItem_Click(object sender, EventArgs e)
+    {
+        Form frm = new Form();
+        frm.Text = clickedNode;
+        frm.ShowDialog(this);
+        clickedNode = "";
+    }
+
+    private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+    {
+        if (e.Button == MouseButtons.Right)
         {
-            InitializeComponent();
+            clickedNode = e.Node.Name;
+            mnu.Show(treeView2,e.Location);
         }
     }
+    
+        private void treeView2_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                // Display context menu for eg:
+                TreeNode destinationNode = ((TreeView)sender).GetNodeAt(new Point(e.X, e.Y));
+                treeView2.SelectedNode = destinationNode;
+                contextMenuStrip1.Show(treeView2, e.Location);
+            }
+        }
+
+        private void stripMenuItemAdd_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("اضافه شد");
+        }
+
+        //stripMenuItemAdd_Click
+
+
+        private void stripMenuItemRename_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("تغیر نام پیدا کرد");
+        }
+
+        //stripMenuItemRename_Click
+
+        private void stripMenuItemRemove_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("حذف شد");
+        }
+
+        private void tabMain_Click(object sender, EventArgs e)
+        {
+
+        }
+        //stripMenuItemRemove_Click
+    }
+
+
+
+
+
+
+
+
+
 }
